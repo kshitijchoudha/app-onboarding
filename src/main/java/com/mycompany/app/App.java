1344c78876f5252e82744857f0603c969d30e6b1
KC <kchoudha@gmail.com>
2022-12-19 20:25:36 -0600

    several fixes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,47 @@
 package com.mycompany.app;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+   
+    public static void main(String[] args) throws Exception {
+        System.out.println("Application onboarding on Harness and Github....");
+
+        String serviceName;
+
+        if (args == null || args.length == 0)
+            serviceName = "delete-me-service";
+        else
+            serviceName = args[0];
+        String envName = serviceName + "-env-1";
+        String infraDefName = envName + "-infra-def-1";
+        String pipelineName = serviceName + "-pipeline-1";
+        HarnessClient harnessClient = new HarnessClient();
+        harnessClient.createHarnessEnvironment(envName);
+
+        harnessClient.createHarnessInfraDefinition(infraDefName,
+                envName.replace("-", ""), "NativeHelm", "default");
+        // harnessClient.createHarnessK8SConnector("auto-k8s-connector-kind","k8s-kind-mac-delegate");
+
+        GitHubClient ghClient = new GitHubClient();
+        System.out.println("Creating Github Repo....");
+        ghClient.createSpringBootHelmRepo(serviceName);
+
+        //TODO: find a better way to identify when new repo is ready for new commit
+        Thread.sleep(20000);
+
+        harnessClient.createHarnessService(serviceName, serviceName);
+
+        harnessClient.createHarnessPipeline(pipelineName,
+                serviceName.replace("-", ""),
+                envName.replace("-", ""),
+                infraDefName.replace("-", ""),
+                serviceName);
     }
 }
--- src/main/java/com/mycompany/app/HarnessClient.java
+++ src/main/java/com/mycompany/app/HarnessClient.java
@@ -1,9 +1,10 @@
 package com.mycompany.app;
+
 import java.io.IOException;
 import java.net.*;
 import java.net.http.*;
-import java.util.*;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HarnessClient {
@@ -11,28 +12,31 @@ public class HarnessClient {
   private static final String accountId = "9S4tqOcyQkeFXybZvJt6fA";
   private static final String orgId = "default";
   private static final String projectId = "Default_Project_1671068007356";
+  // private final static Logger LOGGER = Logger.getAnonymousLogger();
+
   public static void main(String[] args) throws Exception {
 
+    String serviceName = "delete-this-3";
+    String envName = serviceName + "-env-1";
+    String infraDefName = envName + "-infra-def-1";
+    String pipelineName = serviceName + "-pipeline-2";
     HarnessClient harnessClient = new HarnessClient();
-    //harnessClient.createHarnessEnvironment("auto-env-1");
-    harnessClient.createHarnessInfraDefinition("auto-infra-def-env-1", "autoenv1", "NativeHelm", "default");
-    //harnessClient.createHarnessK8SConnector("auto-k8s-connector-kind", "k8s-kind-mac-delegate");
-    
+    harnessClient.createHarnessPipeline(pipelineName,
+                serviceName.replace("-", ""),
+                envName.replace("-", ""),
+                infraDefName.replace("-", ""),
+                serviceName);
+
   }
 
-  public String createHarnessEnvironment(String envName) throws IOException, InterruptedException{
+  public String createHarnessEnvironment(String envName) throws IOException, InterruptedException {
 
     var httpClient = HttpClient.newBuilder().build();
 
-    var payload = String.join("\n"
-      , "{"
-      , " \"orgIdentifier\": \""+HarnessClient.orgId+"\","
-      , " \"projectIdentifier\": \""+HarnessClient.projectId+"\","
-      , " \"identifier\": \""+envName.replace("-", "")+"\","
-      , " \"name\": \""+envName+"\","
-      , " \"type\": \"PreProduction\""
-      , "}"
-    );
+    var payload = String.join("\n", "{", " \"orgIdentifier\": \"" + HarnessClient.orgId + "\",",
+        " \"projectIdentifier\": \"" + HarnessClient.projectId + "\",",
+        " \"identifier\": \"" + envName.replace("-", "") + "\",", " \"name\": \"" + envName + "\",",
+        " \"type\": \"PreProduction\"", "}");
 
     System.out.println(payload);
 
@@ -40,17 +44,17 @@ public class HarnessClient {
     params.put("accountIdentifier", HarnessClient.accountId);
 
     var query = params.keySet().stream()
-      .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
-      .collect(Collectors.joining("&"));
+        .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
 
     var host = "https://app.harness.io";
     var pathname = "/gateway/ng/api/environmentsV2";
     var request = HttpRequest.newBuilder()
-      .POST(HttpRequest.BodyPublishers.ofString(payload))
-      .uri(URI.create(host + pathname + '?' + query))
-      .header("Content-Type", "application/json")
-      .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
-      .build();
+        .POST(HttpRequest.BodyPublishers.ofString(payload))
+        .uri(URI.create(host + pathname + '?' + query))
+        .header("Content-Type", "application/json")
+        .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
+        .build();
 
     var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
@@ -59,50 +63,41 @@ public class HarnessClient {
     return response.body();
   }
 
-  public String createHarnessInfraDefinition(String infraDefName, String envRef, String deploymentType, String nameSpace) throws IOException, InterruptedException{
+  public String createHarnessInfraDefinition(String infraDefName, String envRef, String deploymentType,
+      String nameSpace) throws IOException, InterruptedException {
 
     var httpClient = HttpClient.newBuilder().build();
 
     var payload = String.join("",
-      "{"
-      , " \"identifier\": \""+infraDefName.replace("-", "")+"\","
-      , " \"orgIdentifier\": \""+HarnessClient.orgId+"\","
-      , " \"projectIdentifier\": \""+HarnessClient.projectId+"\","
-      , " \"environmentRef\": \""+envRef+"\","
-      , " \"name\": \""+infraDefName+"\","
-      , " \"type\": \"KubernetesDirect\","
-      , " \"deploymentType\": \""+deploymentType+"\","
-      , " \"yaml\": \"infrastructureDefinition:\\n"
-      ," name: test-1\\n identifier: test1\\n description: \\\"\\\"\\n"
-      ," tags: {}\\n orgIdentifier: default\\n projectIdentifier: Default_Project_1671068007356\\n"
-      ," environmentRef: "+envRef+"\\n"
-      ," deploymentType: "+deploymentType+"\\n"
-      ," type: KubernetesDirect\\n"
-      ," spec:\\n"
-      ,"  connectorRef: k8skindconnector\\n"
-      ,"  namespace: "+nameSpace+"\\n"
-      ,"  releaseName: release-<+INFRA_KEY>\\n"
-      ," allowSimultaneousDeployments: false\\n\""
-      , "}"
-    );
-    
+        "{", " \"identifier\": \"" + infraDefName.replace("-", "") + "\",",
+        " \"orgIdentifier\": \"" + HarnessClient.orgId + "\",",
+        " \"projectIdentifier\": \"" + HarnessClient.projectId + "\",", " \"environmentRef\": \"" + envRef + "\",",
+        " \"name\": \"" + infraDefName + "\",", " \"type\": \"KubernetesDirect\",",
+        " \"deploymentType\": \"" + deploymentType + "\",", " \"yaml\": \"infrastructureDefinition:\\n",
+        " name: " + infraDefName + "\\n identifier: " + infraDefName.replace("-", "") + "\\n description: \\\"\\\"\\n",
+        " tags: {}\\n orgIdentifier: default\\n projectIdentifier: Default_Project_1671068007356\\n",
+        " environmentRef: " + envRef + "\\n", " deploymentType: " + deploymentType + "\\n",
+        " type: KubernetesDirect\\n", " spec:\\n", "  connectorRef: k8skindconnector\\n",
+        "  namespace: " + nameSpace + "\\n", "  releaseName: release-<+INFRA_KEY>\\n",
+        " allowSimultaneousDeployments: false\\n\"", "}");
+
     System.out.println(payload);
 
     HashMap<String, String> params = new HashMap<>();
     params.put("accountIdentifier", HarnessClient.accountId);
 
     var query = params.keySet().stream()
-      .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
-      .collect(Collectors.joining("&"));
+        .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
 
     var host = "https://app.harness.io";
     var pathname = "/gateway/ng/api/infrastructures";
     var request = HttpRequest.newBuilder()
-      .POST(HttpRequest.BodyPublishers.ofString(payload))
-      .uri(URI.create(host + pathname + '?' + query))
-      .header("Content-Type", "application/json")
-      .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
-      .build();
+        .POST(HttpRequest.BodyPublishers.ofString(payload))
+        .uri(URI.create(host + pathname + '?' + query))
+        .header("Content-Type", "application/json")
+        .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
+        .build();
 
     var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
@@ -110,47 +105,188 @@ public class HarnessClient {
     return response.body();
   }
 
-  public String createHarnessK8SConnector(String connectorName, String delegateSelector) throws IOException, InterruptedException{
+  public String createHarnessK8SConnector(String connectorName, String delegateSelector)
+      throws IOException, InterruptedException {
     var httpClient = HttpClient.newBuilder().build();
 
-    var payload = String.join("\n"
-      , "{"
-      , " \"connector\": {"
-      , "  \"name\": \""+connectorName+"\","
-      , "  \"identifier\": \""+connectorName.replace("-", "")+"\","
-      , "  \"description\": \"k8s-connctor\","
-      , "  \"orgIdentifier\": \""+HarnessClient.orgId+"\","
-      , "  \"projectIdentifier\": \""+HarnessClient.projectId+"\","
-      , "  \"tags\": {"
-      , "   \"servicename\": \"autotest\""
-      , "  },"
-      , "  \"type\": \"K8sCluster\","
-      , "  \"spec\": {"
-      , "   \"connectorType\": {\"type\": \"KubernetesClusterConfig\"},"
-      , "   \"credential\": {\"type\": \"InheritFromDelegate\"},"
-      , "   \"delegateSelectors\": [\""+delegateSelector+"\"]"
-      , "  }"
-      , " }"
-      , "}"
-    );
+    var payload = String.join("\n", "{", " \"connector\": {", "  \"name\": \"" + connectorName + "\",",
+        "  \"identifier\": \"" + connectorName.replace("-", "") + "\",", "  \"description\": \"k8s-connctor\",",
+        "  \"orgIdentifier\": \"" + HarnessClient.orgId + "\",",
+        "  \"projectIdentifier\": \"" + HarnessClient.projectId + "\",", "  \"tags\": {",
+        "   \"servicename\": \"autotest\"", "  },", "  \"type\": \"K8sCluster\",", "  \"spec\": {",
+        "   \"connectorType\": {\"type\": \"KubernetesClusterConfig\"},",
+        "   \"credential\": {\"type\": \"InheritFromDelegate\"},",
+        "   \"delegateSelectors\": [\"" + delegateSelector + "\"]", "  }", " }", "}");
 
     System.out.println(payload);
     HashMap<String, String> params = new HashMap<>();
     params.put("accountIdentifier", HarnessClient.accountId);
-    
 
     var query = params.keySet().stream()
-      .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
-      .collect(Collectors.joining("&"));
+        .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
 
     var host = "https://app.harness.io";
     var pathname = "/gateway/ng/api/connectors";
     var request = HttpRequest.newBuilder()
-      .POST(HttpRequest.BodyPublishers.ofString(payload))
-      .uri(URI.create(host + pathname + '?' + query))
-      .header("Content-Type", "application/json")
-      .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
-      .build();
+        .POST(HttpRequest.BodyPublishers.ofString(payload))
+        .uri(URI.create(host + pathname + '?' + query))
+        .header("Content-Type", "application/json")
+        .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
+        .build();
+
+    var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println(response.body());
+
+    return response.body();
+
+  }
+
+  public String createHarnessService(String serviceName, String gitHubRepoName)
+      throws IOException, InterruptedException {
+    var httpClient = HttpClient.newBuilder().build();
+
+    var payload = String.format("""
+          identifier: %s
+          orgIdentifier: default
+          projectIdentifier: Default_Project_1671068007356
+          name: %s
+          yaml: |
+            service:
+              name: %s
+              identifier: %s
+              tags: {}
+              serviceDefinition:
+                spec:
+                  manifests:
+                    - manifest:
+                        identifier: %s
+                        type: HelmChart
+                        spec:
+                          store:
+                            type: Github
+                            spec:
+                              connectorRef: githubhelmrepo
+                              gitFetchType: Branch
+                              folderPath: /
+                              repoName: %s
+                              branch: main
+                          skipResourceVersioning: false
+                          helmVersion: V3
+                type: NativeHelm
+        """, serviceName.replace("-", ""), serviceName, serviceName, serviceName.replace("-", ""),
+        serviceName + "-helm-chart", gitHubRepoName);
+
+    System.out.println(payload);
+
+    HashMap<String, String> params = new HashMap<>();
+    params.put("accountIdentifier", HarnessClient.accountId);
+
+    var query = params.keySet().stream()
+        .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
+
+    var host = "https://app.harness.io";
+    var pathname = "/gateway/ng/api/servicesV2";
+    var request = HttpRequest.newBuilder()
+        .POST(HttpRequest.BodyPublishers.ofString(payload))
+        .uri(URI.create(host + pathname + '?' + query))
+        .header("Content-Type", "application/yaml")
+        .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
+        .build();
+
+    var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    System.out.println(response.body());
+
+    return response.body();
+
+  }
+
+  public String createHarnessPipeline(String pipelineName, String serviceId, String environmentId, String infraDefId,
+      String gitRepoName) throws IOException, InterruptedException {
+
+    var httpClient = HttpClient.newBuilder().build();
+
+    HashMap<String, String> params = new HashMap<>();
+    params.put("accountIdentifier", HarnessClient.accountId);
+    params.put("orgIdentifier", "default");
+    params.put("projectIdentifier", HarnessClient.projectId);
+    params.put("branch", "main");
+    params.put("repoIdentifier", "");
+    params.put("rootFolder", "/");
+    params.put("filePath", ".harness/" + pipelineName + ".yaml");
+    params.put("commitMsg", "Commit from app onboarding automation");
+    params.put("isNewBranch", "false");
+    params.put("baseBranch", "main");
+    params.put("connectorRef", "githubhelmrepo");
+    params.put("storeType", "REMOTE");
+    params.put("repoName", gitRepoName);
+
+    var payload = String.format("""
+        pipeline:
+          name: %s
+          identifier: %s
+          projectIdentifier: %s
+          orgIdentifier: default
+          tags: {}
+          stages:
+            - stage:
+                name: helm-deploy
+                identifier: helmdeploy
+                description: ""
+                type: Deployment
+                spec:
+                  deploymentType: NativeHelm
+                  service:
+                    serviceRef: %s
+                  environment:
+                    environmentRef: %s
+                    deployToAll: false
+                    infrastructureDefinitions:
+                      - identifier: %s
+                  execution:
+                    steps:
+                      - step:
+                          name: Helm Deployment
+                          identifier: helmDeployment
+                          type: HelmDeploy
+                          timeout: 10m
+                          spec:
+                            skipDryRun: false
+                    rollbackSteps:
+                      - step:
+                          name: Helm Rollback
+                          identifier: helmRollback
+                          type: HelmRollback
+                          timeout: 10m
+                          spec: {}
+                tags: {}
+                failureStrategies:
+                  - onFailure:
+                      errors:
+                        - AllErrors
+                      action:
+                        type: StageRollback
+
+            """, pipelineName, pipelineName.replace("-", ""),
+        HarnessClient.projectId, serviceId, environmentId,
+        infraDefId);
+
+    System.out.println(payload);
+    var query = params.keySet().stream()
+        .map(key -> key + "=" + URLEncoder.encode(params.get(key), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
+
+    var host = "https://app.harness.io";
+    var pathname = "/gateway/pipeline/api/pipelines/v2";
+    var request = HttpRequest.newBuilder()
+        .POST(HttpRequest.BodyPublishers.ofString(payload))
+        .uri(URI.create(host + pathname + '?' + query))
+        .header("Content-Type", "application/yaml")
+        .header("x-api-key", System.getenv("HARNESS_API_TOKEN"))
+        .build();
 
     var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
